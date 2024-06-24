8fc9b76cbbca4a1466ac37e75766fce387906ba2
matdave <mat@matdave.com>
2024-06-24 09:40:42 -0500

    initial commit

---
+++ src/Service.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace MatDave\MODXPackage;
+
+class Service
+{
+    public const VERSION = '0.0.1';
+
+    public $modx = null;
+
+    public $namespace = 'modxpackage';
+
+    public $options = [];
+
+    public function __construct(&$modx, array $options = [])
+    {
+        $this->modx = $modx;
+
+        $corePath = $this->getOption('core_path', $options, $this->modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/' . $this->namespace . '/');
+        $assetsPath = $this->getOption('assets_path', $options, $this->modx->getOption('assets_path', null, MODX_ASSETS_PATH) . 'components/' . $this->namespace . '/');
+        $assetsUrl = $this->getOption('assets_url', $options, $this->modx->getOption('assets_url', null, MODX_ASSETS_URL) . 'components/' . $this->namespace . '/');
+
+        /* loads some default paths for easier management */
+        $this->options = array_merge([
+            'namespace' => $this->namespace,
+            'corePath' => $corePath,
+            'modelPath' => $corePath . 'src/Model/',
+            'chunksPath' => $corePath . 'elements/chunks/',
+            'snippetsPath' => $corePath . 'elements/snippets/',
+            'templatesPath' => $corePath . 'templates/',
+            'assetsPath' => $assetsPath,
+            'assetsUrl' => $assetsUrl,
+            'jsUrl' => $assetsUrl . 'mgr/js/',
+            'cssUrl' => $assetsUrl . 'mgr/css/',
+            'connectorUrl' => $this->modx->getOption('connector_url'),
+            'version' => self::VERSION
+        ], $options);
+        $this->addPackage();
+        $this->modx->lexicon->load($this->namespace . ':default');
+    }
+
+    public function addPackage()
+    {
+        // Empty function for extending to modx 2.x
+    }
+
+    /**
+     * Get a local configuration option or a namespaced system setting by key.
+     *
+     * @param string $key The option key to search for.
+     * @param array $options An array of options that override local options.
+     * @param mixed $default The default value returned if the option is not found locally or as a
+     * namespaced system setting; by default this value is null.
+     * @return mixed The option value or the default value specified.
+     */
+    public function getOption($key, $options = [], $default = null)
+    {
+        $option = $default;
+        if (!empty($key) && is_string($key)) {
+            if ($options != null && array_key_exists($key, $options)) {
+                $option = $options[$key];
+            } elseif (array_key_exists($key, $this->options)) {
+                $option = $this->options[$key];
+            } elseif (array_key_exists("{$this->namespace}.{$key}", $this->modx->config)) {
+                $option = $this->modx->getOption("{$this->namespace}.{$key}");
+            }
+        }
+        return $option;
+    }
+}
